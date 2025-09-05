0aa8bea226a5bc0435c75a23a9a7bce21de1f0a4
AdamNandor <144881211+AdamNandor@users.noreply.github.com>
2025-09-05 15:37:46 +0200

    Teljes program

--- kocsik.py
+++ kocsik.py
@@ -10,6 +10,28 @@ with open("kocsik.txt", "r", encoding="UTF-8") as f:
             "loero": int(loero),
             "nyomatek_nm": int(nyomatek)
         })
-
+# összes autó
+print("Összes autó:")
 for auto in autok:
     print(f"{auto['marka']} {auto['modell']}: {auto['loero']} LE, {auto['nyomatek_nm']} Nm, {auto['ar_eur']} EUR")
+# Legdrágább autó
+legdragabb = autok[0]
+for auto in autok:
+    if auto["ar_eur"] > legdragabb["ar_eur"]:
+        legdragabb = auto
+print("\nLegdrágább autó:", legdragabb["marka"], legdragabb["modell"], "-", legdragabb["ar_eur"], "EUR")
+
+# Legjobb ár-érték arányú autó
+
+for auto in autok:
+    auto["ar_per_loero"] = auto["ar_eur"] / auto["loero"]
+
+legjobb = min(autok, key=lambda x: x["ar_per_loero"])
+
+print(f"\nLegjobb vétel: {legjobb['marka']} {legjobb['modell']} "
+      f"({legjobb['ar_eur']} EUR, {legjobb['loero']} LE) "
+      f"-> {legjobb['ar_per_loero']:.0f} EUR/LE\n")
+# Eredmények kiírása
+with open("eredmenyek.txt", "w", encoding="UTF-8") as kimenet:
+    kimenet.write(f"Legdrágább autó:{legdragabb["marka"], legdragabb["modell"], "-", legdragabb["ar_eur"], "EUR"}\n")
+    kimenet.write(F"Legjobb vétel: {legjobb['marka']} {legjobb['modell']} ({legjobb['ar_eur']} EUR, {legjobb['loero']} LE)")
